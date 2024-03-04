81fb53d4082db760713e42f3208f234b9977b6af
AndreiListapp <andrei.carmici@listapp.eu>
2024-03-04 18:00:35 +0200

    modifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import store from "./store";
+import LoginPage from "./pages/login.page";
+import NotFoundPage from "./pages/not-found.page";
+import InvoicesPage from "./pages/invoices.page";
+import BillsPage from "./pages/bills.page";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    Component: LoginPage,
+  },
+  {
+    path: "/register",
+    Component: LoginPage,
+  },
+  {
+    path: "/bills",
+    Component: BillsPage,
+  },
+  {
+    path: "/invoices",
+    Component: InvoicesPage,
+  },
+  {
+    path: "*",
+    Component: NotFoundPage,
+  },
+]);
 
 export default function App(): JSX.Element {
-  const [count, setCount] = useState(0);
-
   return (
     <Provider store={store}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </Provider>
   );
 }
