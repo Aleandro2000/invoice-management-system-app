062b58617e3e93e3426785ac4b618f73403c853e
AndreiListapp <andrei.carmici@listapp.eu>
2024-03-05 13:47:42 +0200

    fixes

--- client/src/pages/login.page.tsx
+++ client/src/pages/login.page.tsx
@@ -32,15 +32,15 @@ export default function LoginPage(): JSX.Element {
             {
               ({ errors, touched }) => (
                 <Form className="mx-auto py-12 max-w-[400px]">
-                  <Field type="email" className="w-full p-3 my-2 bg-transparent text-black rounded-lg border-2 border-ass-black-3 focus:border-black duration-300" placeholder="Email" name="email" />
+                  <Field type="email" className="w-full p-3 my-2 bg-transparent text-black rounded-lg border-2 border-black focus:border-black duration-300" placeholder="Email" name="email" />
                   {
                     errors?.email && touched?.email && <AlertTemplate message={errors?.email} />
                   }
-                  <Field type="password" className="w-full p-3 my-2 bg-transparent text-black rounded-lg border-2 border-ass-black-3 focus:border-black duration-300" placeholder="Password" name="password" />
+                  <Field type="password" className="w-full p-3 my-2 bg-transparent text-black rounded-lg border-2 border-black focus:border-black duration-300" placeholder="Password" name="password" />
                   {
                     errors?.password && touched?.password && <AlertTemplate message={errors?.password} />
                   }
-                  <button disabled={loading} className="w-full my-5 p-3 bg-gradient-to-b from-ass-black-1 to-ass-black-2 text-white rounded-full" type="submit">
+                  <button disabled={loading} className="w-full my-5 p-3 bg-gradient-to-b from-ass-black-1 to-ass-black-2 text-black rounded-full" type="submit">
                     {!loading ? <FontAwesomeIcon className="mr-1" icon={faSignIn} size="1x" /> : <SpinnerTemplate />} Sign In
                   </button>
                   <div className="text-center my-1 underline">
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,10 @@ export default {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
-  plugins: [],
 };
